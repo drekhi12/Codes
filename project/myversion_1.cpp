0d9dc205cc8721f9ca6a53bd6b91a02919590fe6
drekhi12 <drekhi12@gmail.com>
2016-11-01 23:07:48 +0530

    update

--- project/myversion_1.cpp
+++ project/myversion_1.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std;
+int row,col;
+int matrix[100][100];
+void print();
 main()
 {
-	int row,col;
 	cout<<"\nEnter no. of people/machines(rows): ";
 	cin>>row;
 	cout<<"Enter no. of jobs(cols): ";
 	cin>>col;
-	int matrix[row][col],matrix1[row][col];
+	int matrix1[row][col];
 	for(int i=0; i<row; i++)
 	{
 		cout<<"\nEnter for person/machine "<<char(65+i)<<endl;
@@ -18,6 +20,10 @@ main()
 			matrix1[i][j]=matrix[i][j];
 		}
 	}
+	print();
+	
+}
+void print(){
 	for(int i=0; i<row; i++)
 	{
 		for(int j=0; j<col; j++)
