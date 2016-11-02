3952ef3ac51fd6cb8a72587dbea0a85b412621d4
drekhi12 <drekhi12@gmail.com>
2016-11-02 17:18:33 +0530

    pat training

---
+++ assessment/number_validator.c
@@ -0,0 +1,20 @@
+main(){
+	char a[20];
+	int i,length=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++){
+		if((int)a[i]>=0&&(int)a[i]<11){
+			printf("Please put digits instead of characters\n");
+			exit(0);
+		}
+		length++;
+	}
+	if(length>10){
+		printf("Put a 10 digit number\n");
+		exit(0);
+	}
+	if(a[0]=='7'||a[0]=='8'||a[0]=='9')
+	printf("Number Excepted\n");
+	else
+	printf("Number Not Excepted\n");
+}
